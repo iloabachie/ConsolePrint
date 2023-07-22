8eda2e40f1351592d9b57fcf88f17040820ea748
Udemezue Iloabachie <udemezue@gmail.com>
2023-07-21 21:02:31 -0400

    add regular expressions to filter format argument and add custom exceptions

--- src/ConsolePrint/animate.py
+++ src/ConsolePrint/animate.py
@@ -1,11 +1,25 @@
 import time
 import os
+import re
 
-if __name__ == "__main__":
-    os.system('cls')
-    print('\033[0m', end="\r")
 
+print('\033[0m', end="\r")
 __terminal_width = os.get_terminal_size().columns
+
+
+class DimensionExceptionError(Exception):
+    """Custom error when the terminal width is too small"""
+    def __init__(self, error_message):
+        # self.error_message = error_message
+        super().__init__(error_message)
+
+
+class FormatArgumentError(Exception):
+    """Incorrect ANSI sequence passed"""
+    def __init__(self, error_message):
+        # self.error_message = error_message
+        super().__init__(error_message)
+
 
 def __ansify_color(color:str):  
     match color:
@@ -31,12 +45,21 @@ def __ansify_color(color:str):
         case 'cyan_bg': color = '\033[46m'
         case 'white_bg': color = '\033[47m'
         case _:
-            if '[' not in color or color[-1] != 'm' or not color[2:3].isdigit():
-                raise Exception("Invalid ANSI escape sequence for argument format")
+            pattern1 = "\\033\[(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])m"
+            pattern2 = "\\033\[48;5;(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])m"            
+            if not (re.fullmatch(pattern1, color) or re.fullmatch(pattern2, color)):
+                raise FormatArgumentError("Invalid ANSI escape sequence for argument format")
     return color
 
-def printing(text:str, delay:float=0.05, style:str='letter', stay:bool=True, rev:bool=False, format:str='default'):
+
+def is_width_ok(*text_length):
+    if __terminal_width <= sum([*text_length]):
+        raise DimensionExceptionError("Terminal width is too small to display text output")
+
+
+def printing(text:str, *, delay:float=0.05, style:str='letter', stay:bool=True, rev:bool=False, format:str='default'):
     """Prints text to console letter by letter or word by word"""
+    is_width_ok(len(text))
     format = __ansify_color(format)
     print(format, end='\r')
     text = text.strip()
@@ -66,8 +89,9 @@ def printing(text:str, delay:float=0.05, style:str='letter', stay:bool=True, rev
     print('\033[0m', end='\r')
 
 
-def flashprint(text:str, blinks:int=5, delay:float=0.2, stay:bool=True, format:str='default'):
+def flashprint(text:str, *, blinks:int=5, delay:float=0.2, stay:bool=True, format:str='default'):
     """Gets printed output to blink"""
+    is_width_ok(len(text))
     format = __ansify_color(format)
     print(format, end='\r')
     text = text.strip()
@@ -79,8 +103,9 @@ def flashprint(text:str, blinks:int=5, delay:float=0.2, stay:bool=True, format:s
     print('\033[0m', end='\r')
 
 
-def flashtext(phrase:str, text:str, index='end', blinks:int=5, delay:float=0.2, format:str='default'):
+def flashtext(phrase:str, text:str, *, index='end', blinks:int=5, delay:float=0.2, format:str='default'):
     """Hilights key word by flashing it"""
+    is_width_ok(len(text), len(phrase), 1)
     format = __ansify_color(format)
     print(format, end='\r')
     textb = ' ' * len(text)
@@ -100,10 +125,11 @@ def flashtext(phrase:str, text:str, index='end', blinks:int=5, delay:float=0.2, 
     print('\033[0m', end='\r')
 
 
-def animate1(text:str, symbol:str="#", format:str='default'):
+def animate1(text:str, *, symbol:str="#", format:str='default'):
     """Flashing masked text to transition to flasing text"""
+    is_width_ok(len(text))
     if len(symbol) != 1:
-        raise Exception("Symbol input should be a single character")
+        raise ValueError("Symbol input should be a single character")
     format = __ansify_color(format)
     text = text.strip()
     symbol = len(text) * symbol
@@ -111,10 +137,11 @@ def animate1(text:str, symbol:str="#", format:str='default'):
     flashprint(text, blinks=2, stay=True, format=format)
 
 
-def animate2(text:str, symbol:str="#", delay:float=0.05, format:str='default'):
+def animate2(text:str, *, symbol:str="#", delay:float=0.05, format:str='default'):
     """Reveals all characters text by text but first masked then flashes"""
+    is_width_ok(len(text))
     if len(symbol) != 1:
-        raise Exception("Symbol input should be a single character")
+        raise ValueError("Symbol input should be a single character")
     format = __ansify_color(format)
     print(format, end='\r')
     text = text.strip()
@@ -125,28 +152,30 @@ def animate2(text:str, symbol:str="#", delay:float=0.05, format:str='default'):
     flashprint(text, blinks=2, stay=True, format=format)
     print('\033[0m', end='\r')
 
-def text_box(text:str, symbol:str="#", spread:bool=False, padding:bool=False, wall:bool=True, align:str="center", format:str='default'):
+
+def text_box(text:str, *, symbol:str="#", spread:bool=False, padding:bool=False, wall:bool=True, align:str|int="center", format:str='default'):
     """Prints text in a box of symbols.
 If the align parameter is a number then the box is indented by the number count"""
     if spread:
         text = " ".join(list(text)).upper()
     if len(symbol) != 1:
-        raise Exception("Symbol input should be a single character")
-    format = __ansify_color(format)
-    print(format, end='\r')
+        raise ValueError("Symbol input should be a single character")
+    format = __ansify_color(format)    
     text = text.strip()
     end = 5 if padding else 3
     text_row = 3 if padding else 2
     length = len(text) + 8
     left_border = text_row - 1  if padding else text_row
     right_border = text_row + 1 if padding else text_row
-    
+    is_width_ok(len(text), 8, align if isinstance(align, int) else 0)
     if align == "left": indent = 0
     elif align == "right": indent = __terminal_width - length
     elif align == "center": indent = __terminal_width//2 - length//2
     elif isinstance(align, int) and align <= (__terminal_width - length): indent = align
-    else: raise Exception(f"Error in the align argument: {align=}")  
+    else: 
+        raise ValueError(f"Error in the align argument: {align=}")  
     
+    print(format, end='\r')
     for row in range(1, end + 1):
         for col in range(1, length + 1):
             if col == 1:
@@ -168,13 +197,15 @@ If the align parameter is a number then the box is indented by the number count"
                 print('\033[0m')
                 
                 
-def star_square(num:int, symbol:str="#", align:str='center', flush:bool=True, format:str='default'):
+def star_square(num:int, *, symbol:str="#", align:str|int='center', flush:bool=True, format:str='default'):
+    is_width_ok(num)
     if len(symbol) != 1:
         raise Exception("Symbol input should be a single character")
     format = __ansify_color(format)
     print(format, end='\r')
     if num < 5 or num > __terminal_width or not isinstance(num, int):
-        raise Exception(f"Invalid square size. Number must be an integer greater than 4 and less than the terminal width: {__terminal_width}")
+        print('\033[0m\r')
+        raise DimensionExceptionError(f"Invalid square size. Number must be an integer greater than 4 and less than the terminal width: {__terminal_width}")
     elif align == 'center':
         indent = '\033[0m' + (' ' * (__terminal_width//2 - num//2)) + format
     elif align == 'right':
@@ -184,7 +215,8 @@ def star_square(num:int, symbol:str="#", align:str='center', flush:bool=True, fo
     elif isinstance(align, int) and __terminal_width - align > num:
         indent = '\033[0m' + (" " * align) + format
     else:
-        raise Exception("Align parameter is invalid")    
+        print('\033[0m\r')
+        raise DimensionExceptionError("Check align parameter and square size with respect to terminal width")    
           
     for row in range(1, num + 1):
         time.sleep(0.04)
@@ -203,7 +235,8 @@ def star_square(num:int, symbol:str="#", align:str='center', flush:bool=True, fo
                 print('\033[0m')
     
 
-def asteriskify(text:str, align:str="center", underscore:bool=True, format:str='default'):
+def asteriskify(text:str, *, align:str="center", underscore:bool=True, format:str='default'):
+    is_width_ok(len(text))
     format = __ansify_color(format)
     print(format, end='\r')
     text = text.strip()
@@ -216,16 +249,18 @@ def asteriskify(text:str, align:str="center", underscore:bool=True, format:str='
     elif align == 'left':
         indent = ''
     else:
+        print('\033[0m\r')
         raise Exception("Align argument error") 
     print(indent + text)
     if underscore:
         print(indent + '*' * length)
     print('\033[0m', end='\r')
     
-    
+
 
 # Code test
 if __name__ == "__main__":
+    os.system('cls')
     printing("hello this should print letter by letter ", delay=0.05, style="letter", stay=True, rev=False, format='red_bg')
     printing("hello this should print word by word but in reverse", delay=0.3, style="word", stay=True, rev=True, format='red')
     flashprint("The entire text should flash", blinks=5, delay=0.2, stay=True, format='green')
